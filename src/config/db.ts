247bde62315a0eb775f0f40446a2e4557ec0bb12
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-10-23 18:24:04 +0200

    fix: checking the url of the database

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,7 @@ import config from "./config";
 
 export default async function connect_db() {
     try {
+        console.log("connect", config.mongodb_uri);
         const client: MongoClient = await MongoClient.connect(config.mongodb_uri!);
         const db: Db = await client.db("Backend");
         return {client, db};
