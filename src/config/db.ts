f882dbe1cf3b3b0a88b2453653936bf1d167e52e
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-10-23 18:27:19 +0200

    fix: db error

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,6 @@ import config from "./config";
 
 export default async function connect_db() {
     try {
-        console.log("connect", config.mongodb_uri);
         const client: MongoClient = await MongoClient.connect(config.mongodb_uri!);
         const db: Db = await client.db("Backend");
         return {client, db};
