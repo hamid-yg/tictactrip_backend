7ffade4161b25d3f27ea53c12a785efa8c1346d2
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-10-23 18:32:03 +0200

    fix: boot timeout on heroku  by adding PORT to env

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express, { Express, Response} from 'express';
+import swaggerUi from 'swagger-ui-express';
+import bodyParser from 'body-parser';
 import justifyRoute from './src/routes/justify.route';
 import authRoute from './src/routes/auth.route';
-import bodyParser from 'body-parser';
 import connect_db from './src/config/db';
-import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger-output.json';
+import config from './src/config/config';
+
 
 const app: Express = express();
 
@@ -23,6 +25,6 @@ app.use('/api/token', authRoute);
 
 app.use('/api/justify', justifyRoute);
 
-app.listen(5000, () => {
+app.listen(config.port, () => {
     console.log("Server is running at http://localhost:5000");
 })
--- src/config/config.ts
+++ src/config/config.ts
@@ -10,7 +10,7 @@ else if (process.env.NODE_ENV === "development")
   mongodb_uri = process.env.MONGODB_DEV;
 
 export default {
-  port: process.env.PORT,
+  port: process.env.PORT || 5000,
   secret: process.env.SECRET_TOKEN,
   mongodb_uri: mongodb_uri
 };
